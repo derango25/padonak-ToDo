4d8fa9a1bcc5c51fd21ad29b495da0e7c03e1dd2
derango25 <zyko25@gmail.com>
2023-03-21 16:25:20 +0300

    - Добавил форму регистрации

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def signupuser(request):
+
+    return render(request, 'todo/signupuser.html', {'form':UserCreationForm()}) #не писать ссылку на шаблон, только корневая папка и название страницы!
