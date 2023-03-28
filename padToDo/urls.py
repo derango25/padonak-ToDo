f4c74a45f69a4524c21afa48a050931a493c62c6
derango25 <zyko25@gmail.com>
2023-03-28 03:07:27 +0300

    - Logout / реализован выход пользователя

--- padToDo/urls.py
+++ padToDo/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Auth
     path('signup/', views.signupuser, name = 'signupuser'),
+    path('logout/', views.logoutuser, name = 'logoutuser'),
     # ToDos
+    path('', views.home, name = 'home'),
     path('current/', views.currenttodos, name = 'currenttodos'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
+def home(request):
+    return render(request, 'todo/home.html')
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
@@ -23,5 +25,9 @@ def signupuser(request):
         else:
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
 
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
