23a39af19d3dea51fc3c6068d2ec5438bb328440
derango25 <zyko25@gmail.com>
2023-04-21 00:46:49 +0300

    - Удаление и редактирование заметок

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, 'todo/home.html')
@@ -72,3 +73,16 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error':'BAD info!'})
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk = todo_pk, user = request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk = todo_pk, user = request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
